2575e8fe6e75aa42e91c98678ee39e7ad5e5088d
nilson <nilson.souto@gonow.com.br>
2011-09-29 16:56:59 -0300

    Discard fragments with low alpha

--- XBPageCurl/Resources/FragmentShader.glsl
+++ XBPageCurl/Resources/FragmentShader.glsl
@@ -6,8 +6,13 @@ varying vec3 v_normal;
 
 void main()
 {
+    vec4 color = texture2D(s_tex, v_texCoord);
+    
+    if (color.a < 0.01) {
+        discard;
+    }
+
     vec3 n = normalize(v_normal);
     float fFrontFacing = float(gl_FrontFacing);
-    vec4 color = texture2D(s_tex, v_texCoord);
     gl_FragColor = vec4(color.rgb * (n.z * (fFrontFacing-0.5)*2.0) + vec3(0.1,0.1,0.1)*(1.0-fFrontFacing), color.a);
 }
