3a414c657a060e2166f6caca4cb9bef28ab2559d
xissburg <xissburg@gmail.com>
2012-01-28 16:45:10 -0200

    Updates in the simple sample.

--- Sample/SimpleCurlViewController.m
+++ Sample/SimpleCurlViewController.m
@@ -14,12 +14,14 @@
 
 @implementation SimpleCurlViewController
 
-@synthesize messyView, pickerView, curlView, backView;
+@synthesize messyView, pickerView, searchBar, textView, curlView, backView;
 
 - (void)dealloc 
 {
     self.messyView = nil;
     self.pickerView = nil;
+    self.searchBar = nil;
+    self.textView = nil;
     self.curlView = nil;
     self.backView = nil;
     [super dealloc];
@@ -27,11 +29,20 @@
 
 #pragma mark - View lifecycle
 
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    CGRect r = self.messyView.frame;
+    self.curlView = [[[XBCurlView alloc] initWithFrame:r] autorelease];
+}
+
 - (void)viewDidUnload
 {
     [super viewDidUnload];
     self.messyView = nil;
     self.pickerView = nil;
+    self.searchBar = nil;
+    self.textView = nil;
     self.curlView = nil;
     self.backView = nil;
 }
@@ -53,31 +64,22 @@
     return YES;
 }
 
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+{
+    // After a rotation we have to recreate the XBCurlView because the page mesh must be recreated in the right dimensions.
+    CGRect r = self.messyView.frame;
+    self.curlView = [[[XBCurlView alloc] initWithFrame:r] autorelease];
+}
+
 - (IBAction)curlButtonAction:(id)sender
 {
     CGRect r = self.messyView.frame;
-    XBPageCurlView *pageCurlView = [[[XBPageCurlView alloc] initWithFrame:r] autorelease];
     self.curlView = pageCurlView;
     [self.curlView drawViewOnFrontOfPage:self.messyView];
     //[self.curlView drawImageOnNextPage:[UIImage imageNamed:@"appleStore"]];
     self.curlView.opaque = NO; //Transparency on the next page (so that the view behind curlView will appear)
     self.curlView.pageOpaque = YES; //The page to be curled has no transparency
-    
     [self.curlView curlView:self.messyView cylinderPosition:CGPointMake(r.size.width/6, r.size.height/2) cylinderAngle:M_PI/2.4 cylinderRadius:UI_USER_INTERFACE_IDIOM()==UIUserInterfaceIdiomPad? 160: 70 animatedWithDuration:kDuration];
-    
-    pageCurlView.snappingEnabled = YES;
-    
-    XBSnappingPoint *p = [[[XBSnappingPoint alloc] init] autorelease];
-    p.position = CGPointMake(120, 100);
-    p.angle = M_PI/3;
-    p.radius = 40;
-    [pageCurlView.snappingPoints addObject:p];
-    
-    p = [[[XBSnappingPoint alloc] init] autorelease];
-    p.position = CGPointMake(320, 230);
-    p.angle = M_PI/2;
-    p.radius = 10;
-    [pageCurlView.snappingPoints addObject:p];
 }
 
 - (IBAction)uncurlButtonAction:(id)sender
